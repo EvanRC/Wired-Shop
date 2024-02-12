4d8001b8d146004d1d6b0088388c1bdab2021c31
Evan Carlson <evanrc1@gmail.com>
2024-02-12 14:57:17 -0800

    more changes made to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,7 +34,7 @@ router.get('/:id', (req, res) => {
       },
       {
         model: Tag,
-        attributes: ['id', 'tag-name']
+        attributes: ['id', 'tag_name']
       }
     ]
   })
@@ -45,12 +45,11 @@ router.get('/:id', (req, res) => {
   });
 });
 
-// create new product
 router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      // Check if tagIds is provided and is an array
+      if (Array.isArray(req.body.tagIds) && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -59,12 +58,17 @@ router.post('/', (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
-      res.status(200).json(product);
+      // If no product tags, just respond with the product
+      res.status(201).json(product);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
+    .then((productTagIds) => {
+      // If there was product tags, productTagIds will be an array
+      if (productTagIds) {
+        res.status(201).json(productTagIds);
+      }
+    })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
       res.status(400).json(err);
     });
 });
